17d8d9aee50a0e4d04b375a0c3b450b5e68bba73
Denys Polishchuk <den.polischuk19@gmail.com>
2021-07-05 14:28:38 +0200

    add restart option

--- rofi-docker.sh
+++ rofi-docker.sh
@@ -7,13 +7,18 @@ selected_container=$(docker ps --format "table {{.ID}}:\t[{{.Image}}]\t{{.Names}
 container_options_attach="Attach"
 container_options_stop="Stop"
 container_options_logs="Logs"
+container_options_restart="Restart"
 
 if [[ ! -z $selected_container ]]; then
 	container_id=$(echo $selected_container | cut -d':' -f 1)
-	selected_action=$(echo -e "$container_options_attach\n$container_options_logs\n$container_options_stop" | rofi -dmenu -selected-row 0)
+	selected_action=$(echo -e "$container_options_attach\n$container_options_logs\n$container_options_restart\n$container_options_stop" | rofi -dmenu -selected-row 0)
 	case $selected_action in 
 		$container_options_logs)
 			$TERMINAL_APP -e $SHELL -c "docker exec -it ${container_id} /bin/sh"
+			;;
+		$container_options_restart)
+			msg=$(docker restart $container_id)
+			rofi -e "Message from the docker: $msg"
 			;;
 		$container_options_attach)
 			$TERMINAL_APP -e $SHELL -c "docker logs -f ${container_id}"
