b9864ce06c52d43c2deccd5ba07a72f9cb7f0e6f
Denys Polishchuk <den.polischuk19@gmail.com>
2021-08-10 10:41:39 +0200

    upd

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # Rofi Docker widget
 ## What can it do
 The script uses docker cli to list all the containers that are currently running and uses the output to display rofi menu.
+
 For each container you can perform further actions:
  - Attach
  - Logs (in `-f` mode)
  - Restart
  - Stop
+
+## IMPORTANT
+Script uses Alacritty as a default terminal to run some actions. If you are using different terminal - change the terminalbin according to your setup.
 
 ## Dependencies
  - Docker
--- rofi-docker.sh
+++ rofi-docker.sh
@@ -3,6 +3,10 @@
 #Set default terminal emulator
 TERMINAL_APP=alacritty
 
+function execInTerminal {
+  $TERMINAL_APP -e $SHELL -c "$1"
+}
+
 selected_container=$(docker ps --format "table {{.ID}}:\t[{{.Image}}]\t{{.Names}}" | sed '1d' | rofi -p "Running containers " -dmenu)
 container_options_attach="Attach"
 container_options_stop="Stop"
@@ -14,14 +18,14 @@ if [[ ! -z $selected_container ]]; then
 	selected_action=$(echo -e "$container_options_attach\n$container_options_logs\n$container_options_restart\n$container_options_stop" | rofi -dmenu -selected-row 0)
 	case $selected_action in 
 		$container_options_attach)
-			$TERMINAL_APP -e $SHELL -c "docker exec -it ${container_id} /bin/sh"
+			execInTerminal "docker exec -it ${container_id} /bin/sh"
 			;;
 		$container_options_restart)
 			msg=$(docker restart $container_id)
 			rofi -e "Message from the docker: $msg"
 			;;
 		$container_options_logs)
-			$TERMINAL_APP -e $SHELL -c "docker logs -f ${container_id}"
+			execInTerminal "docker logs -f ${container_id}"
 			;;
 		$container_options_stop)
 			msg=$(docker stop $container_id)
